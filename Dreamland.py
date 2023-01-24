a7dd04e762696ee0b41b97bbbaa605aa9a806952
TrapDarkness <trapdarkness04@gmail.com>
2023-01-23 21:48:41 -0500

    Fixed too many cards in hand

--- Dreamland.py
+++ Dreamland.py
@@ -2,8 +2,11 @@ import math
 def  dreamland(deck_size,  hand_size, desired_card_amount, copies_of_desired_cards):
     sample_space = math.comb(deck_size, hand_size)
     sample_space_mulligan = math.comb(deck_size, hand_size-1)
-    leftover_choices = math.comb(deck_size - desired_card_amount, hand_size - desired_card_amount)
-    leftover_choices_mulligan = math.comb(deck_size - desired_card_amount, hand_size - 1- desired_card_amount)
+    sum_desired_cards = 0
+    for i in copies_of_desired_cards:
+        sum_desired_cards += copies_of_desired_cards[i]
+    leftover_choices = math.comb(deck_size - sum_desired_cards, hand_size - sum_desired_cards)
+    leftover_choices_mulligan = math.comb(deck_size - sum_desired_cards, hand_size - 1- sum_desired_cards)
     if 0 == 1:
         first_hand = leftover_choices / sample_space
         second_hand = (1 - first_hand) * first_hand
@@ -14,8 +17,8 @@ def  dreamland(deck_size,  hand_size, desired_card_amount, copies_of_desired_car
         copies_combinations = 1
         for i in copies_of_desired_cards:
             copies_combinations *= math.comb(int(i.split("th")[1]), copies_of_desired_cards[i])
-            #print("n: " + str(i.split("th")[1]) +  " r: " + str(copies_of_desired_cards[i]) + " This Combination: " + str(math.comb(int(i.split("th")[1]), copies_of_desired_cards[i])) +  " Total So Far: " + str(copies_combinations))
-        #print("Desired Cards: " + str(desired_card_amount) + " Leftover Choices: " + str(leftover_choices) + " Sample Space:  " + str(sample_space))
+            print("n: " + str(i.split("th")[1]) +  " r: " + str(copies_of_desired_cards[i]) + " This Combination: " + str(math.comb(int(i.split("th")[1]), copies_of_desired_cards[i])) +  " Total So Far: " + str(copies_combinations))
+        print("Desired Cards: " + str(desired_card_amount) + " Leftover Choices: " + str(leftover_choices) + " Sample Space:  " + str(sample_space))
         first_hand = (copies_combinations * leftover_choices) / sample_space
         second_hand = (1 - first_hand) * first_hand
         third_hand = (1- second_hand) * (copies_combinations * leftover_choices_mulligan / sample_space_mulligan)
@@ -30,11 +33,15 @@ h = int(input())
 print("Input # of Desired Cards")
 d = int(input())
 c = {}
+sum_c = 0
 for i in range(d):
     print("Input #Copies of Card in Deck, #Copies of Card in Hand")
     answer = input()
     answer = answer.split(",")
     c[str(i) + "th" + answer[0]] = int(answer[1])
+    sum_c += int(answer[1])
+    if sum_c >= h:
+        break
    # print(answer)
    # print(c)
 print(str(dreamland(n,h,d,c)) + "%")
